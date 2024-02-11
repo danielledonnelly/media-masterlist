82ef2abe4b66599a41614eb6d85e2a7823b3db33
danielledonnelly <daniellemarydonnelly@gmail.com>
2024-02-11 15:43:36 -0330

    Co-authored-by: Jillian Breau <jillianbreau@gmail.com>

---
+++ script.js
@@ -0,0 +1,28 @@
+(function(window, document) {
+
+  // code that should be taken care of right away
+
+  window.onload = initialize;
+
+  function initialize (){
+    // the code to be called when the dom has loaded
+    // #document has its nodes
+
+    const theTable = document.getElementById('game-table')
+
+    console.log(theTable)
+
+    const theBody = document.getElementById('game-table-body')
+
+    console.log(theBody)
+
+    //the code below is creating a div element that says hello and attaching it to an existing tbody element.
+
+    //const myTestDiv = document.createElement('div')
+    //const myTestDivText = document.createTextNode('Helloooo')
+    //myTestDiv.append(myTestDivText)
+    //myTestDiv.setAttribute('id', 'my-test-div');
+    //theBody.append(myTestDiv)
+  }
+
+})(window, document);
